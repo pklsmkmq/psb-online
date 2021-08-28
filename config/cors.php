cd2f60b209f98d4d91c67ec34e507dbf881958f1
ihsanabuhanifah <ihsanabuhanifah@gmail.com>
2021-08-29 00:54:09 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => ['*.smkmadinatulquran.sch.id'],
 
-    'allowed_origins_patterns' => ['*'],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
