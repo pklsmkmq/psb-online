cba9bb9bba2ec104781c3363dc21d58bed316525
nanungsr <thenanungsr@gmail.com>
2021-10-05 20:29:54 +0700

    fix konfirmasi tes blade

--- app/Mail/konfirmasites.php
+++ app/Mail/konfirmasites.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class konfirmasi extends Mailable
+class konfirmasites extends Mailable
 {
     use Queueable, SerializesModels;
 
@@ -29,7 +29,7 @@ class konfirmasi extends Mailable
      */
     public function build()
     {
-        return $this->subject('Konfiirmasi Jadwal Tes Online')
+        return $this->subject('Konfirmasi Jadwal Tes Online')
                     ->view('email/konfirmasites');
     }
 }
